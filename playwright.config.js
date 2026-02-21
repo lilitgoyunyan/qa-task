de439d233cb3251d5fccd6bdfe94a40a8a9fbabf
lilitgoyunyan <lilit.goyunyan@gmail.com>
2026-02-21 15:17:13 +0400

    Fix CI browser setup

--- playwright.config.js
+++ playwright.config.js
@@ -16,12 +16,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
